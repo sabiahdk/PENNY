67bf613a8072f1cbde593c2e3fb815790e5e525c
sabiahdk <sabiahmuhd98@gmail.com>
2025-09-06 23:13:49 +0100

    first commit

---
+++ penny/src/Components/Header/Header.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+const Header = () => {
+  return (
+    <div className="bg-indigo-600 py-10 px-15 ">
+      <div className=" flex justify-between  ">
+        <div>
+          <img src="./images/applogo.png" alt="" />
+        </div>
+
+        <div className="flex items-center max-md:hidden">
+          <div className=" flex  text-white font-bold text-[20px] gap-10 cursor-pointer">
+            <h1 className="hover:text-blue-300">login</h1>
+            <h1 className="hover:text-blue-300">Sign up</h1>
+            <div></div>
+            <RxHamburgerMenu className="block md:hidden max-md:" />
+          </div>
+        </div>
+      </div>
+         <div className="mt-20 ml-2">
+          <h1 className="text-6xl font-bold text-white w-[580px]">An all-in-one utility & everyday payment solution.</h1>
+         </div>
+         <div className="mt-5 ml-2 w-[660px] font-bold text-xl text-white">
+          <h1>Penney stands as a secure and reliable platform, streamlining utility bill payments while offering unparalleled features such as gas and tuition payment options. Moreover, it extends its capabilities to assist businesses in enhancing their collections effortlessly. Experience the ease and security of Penney, your all-in-one utility payment solution.</h1>
+          <div>
+            <button className="bg-white text-indigo-600 font-bold px-10 py-8 rounded-[50px]  mt-10 hover:bg-gray-300">Sign up now! it's FREE
+            </button>
+          </div>
+          <div>
+            <img className="" src="./images/banner-phone_1.png " alt="" />
+          </div>
+
+
+
+         </div>
+
+               
+
+
+    </div>
+  );
+};
+
+export default Header;
